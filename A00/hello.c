d31a8a019f6fd42ceebdcd1ef9e5914538ce7005
Mona Lisa <alinen@savvysine.com>
2022-01-11 16:04:41 -0500

    assignment 00

---
+++ A00/hello.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * hello.c
+ * Author: Aline Normoyle
+ * Date: 1/11/2022
+ * Print Hello World
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello World\n");
+  return 0;
+}
