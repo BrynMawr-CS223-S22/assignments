b5e2b90908c860b54a110364d86753841f785b94
alinen <alinen@savvysine.com>
2022-01-26 17:27:58 -0500

    Vim workshop file

---
+++ Vim-Workshop/vim-practice.c
@@ -0,0 +1,88 @@
+/*
+ * Put your name and motto here
+ */
+include <stdio.h>
+
+int main() {
+printf("Hello vim")
+int a = 0
+scanf("Enter a number: ");
+if (a < 0) {
+  print("a less than zero");
+} else if (a > 0) {
+  print("a greater than zero");
+} else {
+  print(you win!)
+}
+  
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+kruft at the end of the file....
