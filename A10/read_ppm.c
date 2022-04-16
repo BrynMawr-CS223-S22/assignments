d8e604f0327302d84a61c36bcacd4b80c28ae4ae
Mona Lisa <alinen@savvysine.com>
2022-04-16 17:16:43 -0400

    A10: threads

---
+++ A10/read_ppm.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "read_ppm.h"
+
+struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+
+  // your code here
+  return NULL;
+}
+
+void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+
+  // your code here
+}
+
---
+++ A10/thread_mandelbrot.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <time.h>
+#include <pthread.h>
+#include "read_ppm.h"
+
+int main(int argc, char* argv[]) {
+  int size = 480;
+  float xmin = -2.0;
+  float xmax = 0.47;
+  float ymin = -1.12;
+  float ymax = 1.12;
+  int maxIterations = 1000;
+  int numProcesses = 4;
+
+  int opt;
+  while ((opt = getopt(argc, argv, ":s:l:r:t:b:p:")) != -1) {
+    switch (opt) {
+      case 's': size = atoi(optarg); break;
+      case 'l': xmin = atof(optarg); break;
+      case 'r': xmax = atof(optarg); break;
+      case 't': ymax = atof(optarg); break;
+      case 'b': ymin = atof(optarg); break;
+      case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> "
+        "-b <ymin> -t <ymax> -p <numProcesses>\n", argv[0]); break;
+    }
+  }
+  printf("Generating mandelbrot with size %dx%d\n", size, size);
+  printf("  Num processes = %d\n", numProcesses);
+  printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
+  printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
+
+  // todo: your code here
+  // generate pallet
+  // compute image
+}
